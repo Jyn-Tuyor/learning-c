8f509890a341e23925b819b43a311e2d25461f8f
JynJo <tuyor.jynchris@gmail.com>
2025-08-01 08:33:57 +0800

    reverse print

---
+++ memory_allocation/4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n;
+
+    printf("How much memory would u like to allocate\n");
+    scanf("%d", &n);
+
+    int *ptr = (int *)malloc(n * sizeof(int));
+    int val;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Element - %d : ", i);
+        scanf("%d", &val);
+        ptr[i] = val;
+    }
+
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d, ", ptr[i]);
+    }
+
+    free(ptr);
+
+    return 0;
+}   
